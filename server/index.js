72bf29cbf2e78e9f02a3ca5ad9805b678c6d1ad6
Francis-Mwaniki <fmwaniki986@gmail.com>
2022-05-26 19:36:10 +0300

    modified

--- server/index.js
+++ server/index.js
@@ -5,16 +5,17 @@ const bodyParser=require('body-parser')
 const cors=require('cors')
 const  authRoutes= require('./routes/auth')
 const postRoutes=require('./routes/post')
+const path=require('path')
 //express app
 const app=express();
 //DB connections
-dotenv.config();
-mongoose.connect(process.env.DB_CONNECT,()=>{
-    console.log('connected to the db');
-})
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+mongoose.connect(process.env.DB_CONNECT,{
+   useNewUrlParser: true 
+}).then(()=>{console.log(`connected to db`);}).catch((err)=>{console.log(`ERROR::${err}`);})
 
 //middlewares
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 app.use('/api/user',authRoutes)
 app.use('/api/post',postRoutes)
